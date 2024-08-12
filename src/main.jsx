ee3ffae494d46d7391dc5a24d35a4c31eeae7a51
Serhii Krasovskyi <chokorik@gmail.com>
2024-08-12 10:54:07 +0300

    try-again

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,23 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
 import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <BrowserRouter>
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </StrictMode>
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
@@ -16,5 +30,5 @@ createRoot(document.getElementById("root")).render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );

9562cf94f6c6dc52fb517ad9a2442007f577a997
Serhii Krasovskyi <chokorik@gmail.com>
2024-08-12 10:14:53 +0300

    end

--- src/components/CamperDetails/CamperDetails.jsx
+++ src/components/CamperDetails/CamperDetails.jsx
@@ -8,35 +8,37 @@ const CamperDetails = ({ camper }) => {
   };
 
   return (
-    <ul className={css.characteristics}>
-      <li className={css.listItem}>
-        <Icon iconId="icon-adults" className={css.iconAdults} />
-        {`${camper.adults} adults`}
-      </li>
-      <li className={css.listItem}>
-        <Icon
-          iconId="icon-transmission"
-          className={clsx(css.icon, css.transmission)}
-        />
-        {firstLetterUpper(camper.transmission)}
-      </li>
-      <li className={css.listItem}>
-        <Icon iconId="icon-petrol" className={css.iconPetrol} />
-        {firstLetterUpper(camper.engine)}
-      </li>
-      <li className={css.listItem}>
-        <Icon iconId="icon-kitchen" className={css.icon} />
-        {camper.details.kitchen && `Kitchen`}
-      </li>
-      <li className={css.listItem}>
-        <Icon iconId="icon-beds" className={css.icon} />
-        {`${camper.details.beds} beds`}
-      </li>
-      <li className={css.listItem}>
-        <Icon iconId="icon-ac" className={css.iconAC} />
-        {camper.details.airConditioner && `AC`}
-      </li>
-    </ul>
+    <>
+      <ul className={css.characteristics}>
+        <li className={css.listItem}>
+          <Icon iconId="icon-adults" className={css.iconAdults} />
+          {`${camper.adults} adults`}
+        </li>
+        <li className={css.listItem}>
+          <Icon
+            iconId="icon-transmission"
+            className={clsx(css.icon, css.transmission)}
+          />
+          {firstLetterUpper(camper.transmission)}
+        </li>
+        <li className={css.listItem}>
+          <Icon iconId="icon-petrol" className={css.iconPetrol} />
+          {firstLetterUpper(camper.engine)}
+        </li>
+        <li className={css.listItem}>
+          <Icon iconId="icon-kitchen" className={css.icon} />
+          {camper.details.kitchen && `Kitchen`}
+        </li>
+        <li className={css.listItem}>
+          <Icon iconId="icon-beds" className={css.icon} />
+          {`${camper.details.beds} beds`}
+        </li>
+        <li className={css.listItem}>
+          <Icon iconId="icon-ac" className={css.iconAC} />
+          {camper.details.airConditioner && `AC`}
+        </li>
+      </ul>
+    </>
   );
 };
 
